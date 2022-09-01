d628a9b47bc6f5e4c04d8894db58e77036b3c401
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-09-01 10:20:23 +0600

    Keys, values, entries, delete, seal, freeze

---
+++ key-values.js
@@ -0,0 +1,14 @@
+const bottle = { color: 'yello', price: 50, isClear: true, capasity: 1 }
+const keys = Object.keys(bottle);
+console.log(keys);
+const values = Object.values(bottle);
+console.log(values);
+const pair = Object.entries(bottle);
+console.log(pair);
+
+console.log(bottle);
+//Object.seal(bottle);//seal korle delete kora jabe na, seal korle value change kora jai, new propaty add kora jai na.
+Object.freeze(bottle);
+delete bottle.isClear;
+bottle.price = 200;//seal korle value change kora jai, new propaty add kora jai na.
+console.log(bottle);
